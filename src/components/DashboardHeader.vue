4cd42cee20aff22727322b1ec7bfb0387ab82140
Temidayo325 <59534757+Temidayo325@users.noreply.github.com>
2022-07-08 14:34:30 +0100

    The Adilo test first commit

---
+++ src/components/DashboardHeader.vue
@@ -0,0 +1,159 @@
+<script setup>
+import { reactive, ref } from 'vue'
+const emit = defineEmits(['recording'])
+ // const combined = 25
+ defineProps({
+   combined: Number
+ });
+ let data = reactive({
+      projects: [
+           {name: 'First one', id: 1},
+           {name: 'Second one', id: 2},
+           {name: 'Third one', id: 3},
+           {name: 'Last one', id: 4}
+      ],
+      recordingOptions: {screen: true, video: { facingMode: "user" }, audio: false},
+      showModal: false,
+      chosenProject: ''
+})
+ function showRecordingOptions()
+ {
+     data.showModal = true
+ }
+ function startRecording()
+ {
+      if (navigator.getUserMedia)
+      {
+           let stream = window.navigator.mediaDevices.getUserMedia(data.recordingOptions)
+           .then( request => {
+               // console.log("streaming available")
+               emit('recording', request)
+           })
+           .catch( (err) => {
+                alert(err)
+                console.log(err)
+           })
+      }else{
+           alert("Not available")
+      }
+      
+       
+      // navigator.mediaDevices.getDisplayMedia(data.recordingOptions)
+      // .catch(err => { console.error("Error:" + err); return null; });
+      // navigator.mediaDevices.getUserMedia(data.recordingOptions)
+      // .then( (stream) => {
+      //      emit('recording', stream)
+      //      // console.log(stream)
+      // }).catch( (err) => {
+      //      console.log(err)
+      //      alert(err)
+      // })
+ }
+ function closeModal()
+ {
+      data.showModal = false
+ }
+</script>
+<template>
+       <section class="bg-white py-4 my-2">
+            <div class="flex justify-start items-center gap-20 mx-7">
+                 <div>
+                      <p class="text-sm text-gray-500 my-2 ">Snapbyte > My Recordings</p>
+                      <h1 class="text-2xl">My Recordings {{combined}}</h1>
+                 </div>
+                 <ul class="flex jusify-center items-center gap-6">
+                      <li class="flex justify-start items-center gap-2 border border-gray-200 rounded-full py-2 px-4 cursor-pointer">
+                           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+                           </svg>
+                           <span>By Date</span>
+                      </li>
+                      <li class="flex justify-start items-center gap-2 border border-gray-200 rounded-full py-2 px-4 cursor-pointer">
+                           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
+                           </svg>
+                           <span>Add Filter</span>
+                      </li>
+                      <li class="flex justify-start items-center gap-2 rounded-full py-3 px-6 bg-teal-500 text-white cursor-pointer">
+                           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                          </svg>
+                           <span>New Request</span>
+                      </li>
+                      <li class="flex justify-start items-center gap-2 rounded-full py-3 px-6 bg-red-500 text-white cursor-pointer" @click="showRecordingOptions()" >
+                           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                          </svg>
+                           <span>Start Recording</span>
+                      </li>
+                 </ul>
+            </div>
+       </section>
+       <section >
+            <div id="defaultModal" tabindex="-1" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full backdrop-opacity-90  bg-black/40 flex justify-center items-center" v-if="data.showModal">
+          <div class="relative p-4 w-full max-w-lg h-full md:h-auto">
+             <!-- Modal content -->
+             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                 <!-- Modal header -->
+                 <div class="flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600">
+                     <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+                         New Recording
+                     </h3>
+                     <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal" @click="closeModal()">
+                         <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
+                     </button>
+                 </div>
+                 <!-- Modal body -->
+                 <div class="p-6 space-y-6 w-full mx-auto px-6">
+                     <label for="my-projects">Save the recording in</label>
+                     <select id="my-projects" name="my-projects" v-model="data.chosenProject" class="block bg-gray-100 py-3 rounded-lg px-3 my-2 mx-auto w-full">
+                          <option disbaled value="">Select a project</option>
+                          <option v-for="project in data.projects" :key="project.id" :value="project.name">{{project.name}}</option>
+                     </select>
+                     
+                 </div>
+                 <ul class='w-10/12 mx-auto py-3'>
+                      <li class="flex justify-between items-center">
+                           <p class="font-bold text-sm tracking-wide text-gray-600 ">Record screen</p>
+                           <label for="default-toggle" class="inline-flex relative items-center cursor-pointer">
+                                <div>
+                                     <input type="checkbox" value="" id="default-toggle" class="sr-only peer" :checked="data.recordingOptions.screen" >
+                                     <div class="w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
+                                </div>
+                                
+                         </label>
+                      </li>
+                      <li class="flex justify-between items-center my-6">
+                           <p class="font-bold text-sm tracking-wide text-gray-600">Record Camera</p>
+                           <label for="default-toggle-2" class="inline-flex relative items-center cursor-pointer">
+                                <div>
+                                     <input type="checkbox" value="" id="default-toggle-2" class="sr-only peer" :checked="data.recordingOptions.video" >
+                                     <div class="w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
+                                </div>
+                                
+                         </label>
+                      </li>
+                      <li class="flex justify-between items-center">
+                           <p class="font-bold text-sm tracking-wide text-gray-600 ">Record Mic</p>
+                           <label for="default-toggle-3" class="inline-flex relative items-center cursor-pointer">
+                                <div>
+                                     <input type="checkbox" value="" id="default-toggle-3" class="sr-only peer" :checked="data.recordingOptions.audio">
+                                     <div class="w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
+                                </div>
+                                
+                         </label>
+                      </li>
+                 </ul>
+                 <!-- Modal footer -->
+                 <div class="flex justify-center items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                     <button type="button" class="text-white bg-blue-400 hover:bg-blue-800 focus:ring-4 focus:outline-none rounded-full focus:ring-blue-300 font-medium text-sm px-12 py-2.5 text-center " @click="startRecording()">Start Recording</button>
+                 </div>
+             </div>
+          </div>
+          </div>
+       </section>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,174 @@
+<script setup>
+import { ref , reactive, watchEffect } from 'vue'
+import DashboardHeader from './DashboardHeader.vue'
+
+// defineProps({
+//   msg: String
+// });
+const data = reactive({
+     totalNumber: 25,
+     recordings: [
+          {image: "https://i.pravatar.cc/150?img=4", title: "The title which is usualy longer than this and can be annoying", views: 30, size: "1002 kb", last_modified: "1 month ago", id: 1},
+          {image: "https://i.pravatar.cc/150?img=7", title: "The title which is usualy longer than this and can be annoying", views: 30, size: "1002 kb", last_modified: "2 month ago", id: 2},
+          {image: "https://i.pravatar.cc/150?img=8", title: "The title which is usualy longer than this and can be annoying", views: 30, size: "1002 kb", last_modified: "1 week ago", id: 23},
+          {image: "https://i.pravatar.cc/150?img=9", title: "The title which is usualy longer than this and can be annoying", views: 30, size: "1002 kb", last_modified: "2 weeks ago", id: 24},
+          // {image: "https://i.pravatar.cc/150?img=10", title: "The title which is usualy longer than this and can be annoying", views: 30, size: "1002 kb", last_modified: "1 month ago", id: 54},
+          // {image: "https://i.pravatar.cc/150?img=20", title: "The title which is usualy longer than this and can be annoying", views: 30, size: "1002 kb", last_modified: "4 month ago", id: 12},
+          // {image: "https://i.pravatar.cc/150?img=67", title: "The title which is usualy longer than this and can be annoying", views: 30, size: "1002 kb", last_modified: "3 weeks ago", id: 13},
+          // {image: "https://i.pravatar.cc/150?img=12", title: "The title which is usualy longer than this and can be annoying", views: 30, size: "1002 kb", last_modified: "1 month ago", id: 15}
+     ],
+     screens: {controls: true, filledLibrary: true, video: false, stream: {}}
+})
+ const count = ref(0)
+ const video = ref(null)
+ const user = {
+     name: 'Cody Fisher',
+     email: 'codyfisher@gmail.com'
+ }
+ watchEffect(() => {
+  if (video.value) {
+    console.log("Now available")
+    video.value.srcObject = data.screens.stream
+    video.onloadedmetadata = function(e) {
+       video.play();
+    };
+  } else {
+    console.log("not available")
+  }
+})
+
+ function recordingStarted(value)
+ {
+      data.screens.filledLibrary = false
+      data.screens.stream = value
+      data.screens.video = true
+      
+ }
+</script>
+ <template>
+      <main class="bg-white relative py-0 min-h-min relative" v-if="data.screens.filledLibrary">
+          <header class="flex justify-around items-center border-b-2 border-gray-200 py-6 sticky-0">
+               <div class="flex justify-start items-center gap-3">
+                    <div class="outer-circle w-8 h-8 rounded-xl bg-blue-300 flex justify-center items-center">
+                         <div class="inner-circle h-5 w-5 rounded-full bg-white"></div>
+                    </div>
+                    <h1 class="header font-bold text-2xl inline ">Adilo</h1>
+               </div>
+               <nav class="flex justify-start gap-10 text-gray-800">
+                    <li><a>Projects</a></li>
+                    <li><a>Tools and App</a></li>
+                    <li><a>Channels</a></li>
+                    <li><a>Contacts</a></li>
+                    <li><a>Analytics</a></li>
+                    <li><a>Settings</a></li>
+               </nav>
+               <div class="flex justify-start gap-4 items-center">
+                    <p class="font-bold border-r-2 border-gray-200 px-2">Help</p>
+                    <div class="flex justify-start gap-3">
+                         <img src="https://i.pravatar.cc/150?img=1" alt="user avatar" class="w-12 h-12 rounded-xl cursor-pointer w-12 h-12 ">
+                         <div>
+                              <h3 class="text-left font-bold">{{user.name}}</h3>
+                              <p>{{user.email}}</p>
+                         </div>
+                    </div>
+               </div>
+          </header>
+          <div class="flex justify-start items-start my-0 h-screen" >
+               <section class="bg-blue-100 w-2/12 py-5 border-r-2 border-gray-400 h-full sticky">
+                    <ul class="h-full">
+                         <li class="flex justify-start ml-3 py-3 px-3 gap-8 cursor-pointer">
+                              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                   <path stroke-linecap="round" stroke-linejoin="round" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                              </svg>
+                              <span class="text-md">My Recordings</span>
+                         </li>
+                         <li class="flex justify-start ml-3 py-3 px-3 gap-8 cursor-pointer">
+                              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                   <path stroke-linecap="round" stroke-linejoin="round" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
+                              </svg>
+                              <span>Requested</span>
+                         </li>
+                    </ul>
+               </section>
+               <section class="w-full">
+                    <DashboardHeader :combined='data.totalNumber' v-if="data.screens.controls" @recording="recordingStarted"/>
+                    <div>
+                         <div class="relative overflow-x-auto shadow-md sm:rounded-lg mx-8 my-12">
+                         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs uppercase dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        Recordings
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Title
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Views
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Size
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Last modified
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        <span class="sr-only"></span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="bg-white " v-for="recording in data.recordings" :key="recording.id">
+                                    <th scope="row" class="px-2 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                        <img :src="recording.image" class="w-48 h-24 rounded my-2 cursor-pointer"/>
+                                    </th>
+                                    <td class="px-3 py-4">
+                                        {{recording.title}}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {{recording.views}}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {{recording.size}}
+                                    </td>
+                                    <td class="px-6 py-4 text-right">
+                                        {{recording.last_modified}}
+                                    </td>
+                                    <td class="px-6 py-4 text-right cursor-pointer">
+                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                             <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                                             </svg>
+                                    </td>
+                                </tr>
+                            </tbody>
+                         </table>
+                         </div>
+                    </div>
+               </section>
+          </div>
+      </main>
+      <aside v-if="!data.screens.filledLibrary" class="vid-container py-20 flex justify-center h-screen w-screen">
+           <div class="my-4 w-6/12">
+                <p class="text-left font-bold text-gray-700 mx-4 mb-2">Live preview</p>
+                <div class="vid-screen mx-auto w-full h-3/4 rounded-lg">
+                     <video class="w-full h-full" ref="video" controls autoplay id="video" v-if="data.screens.video"></video>
+                </div>
+                <div class="flex justify-center items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                    <button type="button" class="text-white bg-blue-400 hover:bg-blue-800 focus:ring-4 focus:outline-none rounded-full focus:ring-blue-300 font-medium text-sm px-12 py-3 text-center " @click="startRecording()">Stop Recording</button>
+                </div>
+           </div>
+      </aside>
+ </template>
+<style scoped>
+li{
+     list-style: none;
+}
+.vid-container
+{
+     background-color: #EAFAFF;
+}
+.vid-screen
+{
+     background-color: #21455E;
+}
+</style>
